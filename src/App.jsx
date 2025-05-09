d6a64871461bcc5f1bf14551deda24217f85d68e
Jeremy Dudet <j.dudet@gmail.com>
2025-05-08 21:32:05 -0700

    Refactor App component layout by simplifying class structure for improved styling and responsiveness.

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import { Experience } from '@/components/Experience'
 export default function App() {
   return (
     <Router>
-      <main className="flex flex-1 flex-col pb-2 min-w-0 pt-2 pr-2 pl-2 h-screen min-h-screen overflow-y-hidden">
-        <div className="grow p-6 rounded-lg bg-white ring-1 shadow-xs ring-zinc-950/5 dark:bg-zinc-900 dark:ring-white/10 h-full min-h-full overflow-y-auto ">
+      <main className="flex flex-1 flex-col pb-2 min-w-0 pt-2 pr-2 pl-2 min-h-screen h-full">
+        <div className="grow p-6 rounded-lg bg-white ring-1 shadow-xs ring-zinc-950/5 dark:bg-zinc-900 dark:ring-white/10 h-full min-h-full">
           <div className="mx-auto max-w-6xl my-8">
           <Introduction />
           </div>
